2aaf300a68caf497003ceb4d0c03ad0569823f22
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-16 16:31:52 +0200

    Populated data structure items to list

--- app.js
+++ app.js
@@ -12,24 +12,31 @@ const ItemCtrl = (function(){
     //Data structure
     const data = {
         items: [
-            {}
+            {id: 0, name: "Steak Dinner", calories: 1200}
         ],
         total: 0
     }
     return {
+        getItems: function(){
+            return data.items
+        },
         logData: function(){
             return data;
         }
     }
     })();
 
-
-
-
-
 //UI controller
 const UICtrl = (function(){
-
+    return {
+        populateItemList: function (items){
+            let html = "";
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item ${item.id}"> <strong>${item.name}:</strong> <em>${item.calories} Calories</em> <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a> </li>`
+            })
+            document.querySelector("#item-list").innerHTML = html;
+        }
+    }
 })();
 
 //app controller
@@ -37,6 +44,8 @@ const App = (function(ItemCtrl, UICtrl){
     return {
         init: function(){
             console.log("Initializing app");
+            const items = ItemCtrl.getItems();
+            UICtrl.populateItemList(items);
         }
     }
 })(ItemCtrl, UICtrl);
